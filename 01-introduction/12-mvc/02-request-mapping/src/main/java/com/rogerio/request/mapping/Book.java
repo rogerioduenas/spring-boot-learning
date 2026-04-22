b6763ee7ed32354d9d1f1973b7769ce39c6a2a62
rogerioduenas <rogerioduenasdev@hotmail.com>
2026-04-22 19:53:00 +0900

    feat: implement multiple examples of @requestMapping using shortcuts.

---
+++ 01-introduction/12-mvc/02-request-mapping/src/main/java/com/rogerio/request/mapping/Book.java
@@ -0,0 +1,4 @@
+package com.rogerio.request.mapping;
+
+public record Book(Long id, String title) {
+}
---
+++ 01-introduction/12-mvc/02-request-mapping/src/main/java/com/rogerio/request/mapping/Routes.java
@@ -0,0 +1,85 @@
+package com.rogerio.request.mapping;
+
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+public class Routes {
+
+  // HTTP Method
+  //  @RequestMapping(value = "/ex", method = RequestMethod.GET)
+  //  or only
+  @GetMapping("/ex")
+  public String getSimpleExample() {
+    return "GET example";
+  }
+
+  // HTTP Headers Attribute
+  //  @RequestMapping(value = "/ex/header", headers = "key=val", method = GET)
+  //  or only
+  @GetMapping(value = "/ex/header", headers = "key=val")
+  // for multiple attributes: { "key1=val1", "key2=val2" }
+  public String getExampleWithHeaders() {
+    return "GET example with headers Attribute";
+  }
+
+  // Consumes
+  @PostMapping(value = "/save-book", consumes = "application/json")
+  public String saveBook(@RequestBody Book book) {
+    return book.title() + " book received successfully!";
+  }
+
+  // Produces
+  @GetMapping(value = "/book-json", produces = "application/json")
+  // produces = { "application/json", "application/xml" }
+  public Book getBook() {
+    return new Book(1L, "Spring Boot");
+  }
+
+  // Path Variables
+  @GetMapping(value = "/ex/{id}")
+  public String getSimplePathWithPathVariable(@PathVariable("id") Long id) {
+    return "GET example with path variable {id}: " + id;
+  }
+
+  // Multiple @PathVariable
+  @GetMapping(value = "/ex/{var1}/{var2}")
+  public String getSimplePathWithMultipleVariables(@PathVariable("var1") Long var1, @PathVariable Long var2) {
+    return String.format("var1: %d, var2: %d", var1, var2);
+  }
+
+  // Regex
+  @GetMapping(value = "/ex/regex/{numericId:[\\d]+}")
+  public String getSimplePathWithRegex(@PathVariable long numericId) {
+    return "GET example with Regex: " + numericId;
+  }
+
+  // Request Parameters
+  @GetMapping(value = "/ex/rp") // http://localhost:8080/ex/rp?id=100
+  public String getPathWithRequestParam(@RequestParam long id) {
+    return "GET example with request parameters: " + id;
+  }
+
+  // another way
+  @GetMapping(value = "/ex/rp2", params = "id") // http://localhost:8080/ex/rp2?id=100
+  public String getPathWithRequestParam2(@RequestParam long id) {
+    return "GET example with request parameters: " + id;
+  }
+
+  // Multiple params
+  @GetMapping(value = "/ex/rp3", params = {"id", "second"})
+  // http://localhost:8080/ex/rp3?id=100&second=
+  public String getPathWithMultipleRequestParam(@RequestParam long id) {
+    return "GET example with request parameters: " + id;
+  }
+
+  // Therefore, the 'second' parameter is indeed optional.
+  @GetMapping(value = "/ex/rp4", params = {"id"}) // http://localhost:8080/ex/rp4?id=100
+  public String getPathWithMultipleRequestParam2(
+      @RequestParam long id,
+      @RequestParam(required = false) String second) {
+    return "GET example: " + id + " Second: " + second;
+  }
+
+}
